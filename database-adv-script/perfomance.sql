6f98c50c3e2a7605ac59d921414a629ffd8e6909
ismailouzy <144366402+ismailouzy@users.noreply.github.com>
2025-05-17 19:58:32 +0100

    Create perfomance.sql

---
+++ database-adv-script/perfomance.sql
@@ -0,0 +1,39 @@
+
+SELECT 
+    b.booking_id,
+    b.start_date,
+    b.end_date,
+    b.total_price,
+    u.user_id,
+    u.first_name,
+    u.last_name,
+    p.property_id,
+    p.name AS property_name,
+    pay.payment_id,
+    pay.amount,
+    pay.payment_method
+FROM 
+    Booking b
+JOIN 
+    User u ON b.user_id = u.user_id
+JOIN 
+    Property p ON b.property_id = p.property_id
+LEFT JOIN 
+    Payment pay ON b.booking_id = pay.booking_id;
+
+
+SELECT 
+    b.booking_id,
+    b.start_date,
+    b.end_date,
+    u.first_name || ' ' || u.last_name AS guest_name,
+    p.name AS property_name,
+    pay.amount
+FROM 
+    Booking b
+INNER JOIN 
+    User u ON b.user_id = u.user_id
+INNER JOIN 
+    Property p ON b.property_id = p.property_id
+LEFT JOIN 
+    Payment pay ON b.booking_id = pay.booking_id;
